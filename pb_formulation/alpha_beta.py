d7a03b90ae4f929f082c9313aad84cd150440b65
Stefan Search <stefan.search.14@sansano.usm.cl>
2020-03-16 19:21:29 -0300

    Now returns rhs as two parts and both the internal and external matrices. Additional code tidying.

--- pb_formulation/alpha_beta.py
+++ pb_formulation/alpha_beta.py
@@ -3,7 +3,6 @@ import bempp.api
 
 def laplaceMultitrace(dirichl_space, neumann_space):
     from bempp.api.operators.boundary import laplace
-
     A = bempp.api.BlockedOperator(2, 2)
     A[0, 0] = (-1.0)*laplace.double_layer(dirichl_space, dirichl_space, dirichl_space)
     A[0, 1] = laplace.single_layer(neumann_space, dirichl_space, dirichl_space)
@@ -14,7 +13,6 @@ def laplaceMultitrace(dirichl_space, neumann_space):
 
 def modHelmMultitrace(dirichl_space, neumann_space, kappa):
     from bempp.api.operators.boundary import modified_helmholtz
-
     A = bempp.api.BlockedOperator(2, 2)
     A[0, 0] = (-1.0)*modified_helmholtz.double_layer(dirichl_space, dirichl_space, dirichl_space, kappa)
     A[0, 1] = modified_helmholtz.single_layer(neumann_space, dirichl_space, dirichl_space, kappa)
@@ -24,7 +22,6 @@ def modHelmMultitrace(dirichl_space, neumann_space, kappa):
     return A
 
 def alpha_beta(dirichl_space, neumann_space, q, x_q, ep_in, ep_out, kappa, alpha, beta):
-
     from bempp.api.operators.boundary import sparse
     phi_id = sparse.identity(dirichl_space, dirichl_space, dirichl_space)
     dph_id = sparse.identity(neumann_space, neumann_space, neumann_space)
@@ -59,17 +56,16 @@ def alpha_beta(dirichl_space, neumann_space, q, x_q, ep_in, ep_out, kappa, alpha
     Id[1, 1] = dph_id
 
     A = ((0.5*Id)+A_in)+(D*((0.5*Id)-A_out)*E)-(Id+F)
-    #A = A.strong_form()
 
     def d_green_func(x, n, domain_index, result):
         const = -1./(4.*np.pi*ep_in)
         result[:] = (-1.0)*const*np.sum(q*np.dot( x - x_q, n )/(np.linalg.norm( x - x_q, axis=1 )**3))
+        
 
     def green_func(x, n, domain_index, result):
         result[:] = (-1.0)*np.sum(q/np.linalg.norm( x - x_q, axis=1 ))/(4.*np.pi*ep_in)
 
     rhs_1 = bempp.api.GridFunction(dirichl_space, fun=green_func)
     rhs_2 = bempp.api.GridFunction(dirichl_space, fun=d_green_func)
-    rhs = np.concatenate([rhs_1.coefficients, rhs_2.coefficients])
 
-    return A, rhs
+    return A, rhs_1, rhs_2, A_in, A_out
